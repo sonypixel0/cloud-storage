cdb514eaf677dc29b5431f6e3533df62175f8e8f
sonypixel0 <haidarsidqi429@gmail.com>
2026-02-25 14:58:25 +0700

    storage.js

--- storage.js
+++ storage.js
@@ -1,1 +1,59 @@
+const username = "USERNAME_KAMU";
+const repo = "cloud-storage";
+const token = "TOKEN_KAMU";
 
+async function uploadFile(file) {
+    const reader = new FileReader();
+
+    reader.onload = async function () {
+        const content = reader.result.split(",")[1];
+
+        const response = await fetch(
+            `https://api.github.com/repos/${username}/${repo}/contents/files/${file.name}`,
+            {
+                method: "PUT",
+                headers: {
+                    Authorization: "Bearer " + token,
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    message: "Upload file",
+                    content: content
+                })
+            }
+        );
+
+        if (response.ok) {
+            alert("File berhasil diupload!");
+            listFiles();
+        } else {
+            alert("Upload gagal!");
+        }
+    };
+
+    reader.readAsDataURL(file);
+}
+
+async function listFiles() {
+    const response = await fetch(
+        `https://api.github.com/repos/${username}/${repo}/contents/files`,
+        {
+            headers: {
+                Authorization: "Bearer " + token
+            }
+        }
+    );
+
+    const data = await response.json();
+    const container = document.getElementById("fileList");
+    container.innerHTML = "";
+
+    data.forEach(file => {
+        const div = document.createElement("div");
+        div.innerHTML = `
+            ${file.name}
+            <a href="${file.download_url}" target="_blank">Download</a>
+        `;
+        container.appendChild(div);
+    });
+}
